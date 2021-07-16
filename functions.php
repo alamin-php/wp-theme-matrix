f90b437d18738e807c8a933447e31913d53df552
alamin-php <alamin.php100@gmail.com>
2021-07-16 21:37:14 +0600

    added sidebar wigdets register

--- functions.php
+++ functions.php
@@ -12,3 +12,40 @@ function matrix_assets() {
     // wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'matrix_assets' );
+
+function matrix_sindebar(){
+        register_sidebar( 
+            array(
+            'name'          => __( 'Singel Post Sidebar', 'matrix' ),
+            'id'            => 'sidebar-1',
+            'description'   => __( 'Right Sidebar.', 'matrix' ),
+            'before_widget' => '<section id="%1s" class="widget %2s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widgettitle">',
+            'after_title'   => '</h2>',
+        ) 
+    );
+        register_sidebar( 
+            array(
+            'name'          => __( 'Footer Left', 'matrix' ),
+            'id'            => 'footer-left',
+            'description'   => __( 'Widgets area on footer left-sidebar.', 'matrix' ),
+            'before_widget' => '<div id="%1s" class="widget %2s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '',
+            'after_title'   => '',
+        ) 
+    );        
+    register_sidebar( 
+            array(
+            'name'          => __( 'Footer Right', 'matrix' ),
+            'id'            => 'footer-right',
+            'description'   => __( 'Widgets area on footer right-sidebar.', 'matrix' ),
+            'before_widget' => '<div id="%1s" class="widget %2s text-right">',
+            'after_widget'  => '</div>',
+            'before_title'  => '',
+            'after_title'   => '',
+        ) 
+    );
+}
+add_action( "widgets_init", "matrix_sindebar" );
--- single.php
+++ single.php
@@ -1,52 +1,68 @@
 <?php get_header(  ); ?>
 <body <?php body_class( ); ?>>
 <?php get_template_part( "/template-parts/hero" ); ?>
-<div class="posts">
-<?php if(have_posts(  )) : ?>
-    <?php while(have_posts(  )):the_post(  ); ?>
-        <div class="post" <?php post_class(  ); ?>>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-10 offset-md-1">
-                        <h2 class="post-title text-center"><?php the_title(  ); ?></h2>
-                        <p class="text-center">
-                            <strong><?php the_author(  ); ?></strong><br/>
-                            <?php echo get_the_date( "jS M, Y" ); ?>
-                        </p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-10 offset-md-1">
-
-                        <?php 
-                            if(has_post_thumbnail(  )){
-                                the_post_thumbnail( "learg", array("class" => "img-fluid") );
-                            }else{
-                                ?>
-                                    <p>
-                                        <img class="img-fluid" src="<?php echo get_template_directory_uri(  ) ?>/assets/images/default/no_image.png"
-                                        alt="Post Title">
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="posts">
+            <?php if(have_posts(  )) : ?>
+                <?php while(have_posts(  )):the_post(  ); ?>
+                    <div class="post" <?php post_class(  ); ?>>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h2 class="post-title"><?php the_title(  ); ?></h2>
+                                    <p class="">
+                                        <strong><?php the_author(  ); ?></strong><br/>
+                                        <?php echo get_the_date( "jS M, Y" ); ?>
                                     </p>
-                                <?php
-                            }
-                        ?>
-                        <?php the_content(  );?>
-                        <?php 
-                            next_post_link(  );
-                            echo "</br>";
-                            previous_post_link(  );
-                        ?>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+            
+                                    <?php 
+                                        if(has_post_thumbnail(  )){
+                                            the_post_thumbnail( "learg", array("class" => "img-fluid") );
+                                        }else{
+                                            ?>
+                                                <p>
+                                                    <img class="img-fluid" src="<?php echo get_template_directory_uri(  ) ?>/assets/images/default/no_image.png"
+                                                    alt="Post Title">
+                                                </p>
+                                            <?php
+                                        }
+                                    ?>
+                                    <?php the_content(  );?>
+                                    <?php 
+                                        next_post_link(  );
+                                        echo "</br>";
+                                        previous_post_link(  );
+                                    ?>
+                                </div>
+                                <?php if(comments_open(  )) : ?>
+                                <div class="col-md-10 offset-md-1">
+                                        <?php comments_template(  ); ?>
+                                </div>
+                                <?php endif; ?>
+                            </div>
+            
+                        </div>
                     </div>
-                    <?php if(comments_open(  )) : ?>
-                    <div class="col-md-10 offset-md-1">
-                            <?php comments_template(  ); ?>
-                    </div>
-                    <?php endif; ?>
-                </div>
-
+                <?php endwhile; wp_reset_postdata(  );?>
+            <?php endif; ?>
             </div>
+
+
         </div>
-    <?php endwhile; wp_reset_postdata(  );?>
-<?php endif; ?>
+        <div class="col-md-4">
+            <?php 
+                if(is_active_sidebar( "sidebar-1" )) {
+                    dynamic_sidebar( "sidebar-1" );
+                }
+            
+            ?>
+        </div>
+    </div>
 </div>
 <?php get_footer(  ); ?>
